3f44404d34ea719aee53718341a88d50a42a7d49
Cory Dowdy <cdowdy46@gmail.com>
2023-03-08 14:52:13 -0500

    set up raffleContestants.js to add picked key and do random pick

--- src/stores/raffleContestants.js
+++ src/stores/raffleContestants.js
@@ -7,13 +7,37 @@ export const useContestantStore = defineStore('contestants', {
 
   getters: {
     getContestants (state) {
+      let allState = state.allContestants.map( value => ( {
+        ...value,  picked: false
+      }))
+      this.allContestants = allState
       return state.allContestants
+    },
+    getContestantByNumber (state)  {
+      return ( contestantNumber ) => state.allContestants.find ( ( contestant ) => contestant.numberPurchased === contestantNumber)
+    },
+
+    getChunkedRows (state) {
+      let group = [];
+      let itemsPer = 20;
+      for(var i = 0; i < this.getContestants.length; i += itemsPer) {
+        group.push(this.getContestants.slice(i, i+itemsPer));
+      }
+      return group
     }
   },
 
   actions: {
-    increment () {
-      this.counter++
-    }
-  }
+    picked ( numberToFind ) {
+
+
+      const contestants = this.allContestants.find((obj) => obj.numberPurchased === numberToFind);
+
+      if (contestants) {
+        contestants.picked = !contestants.picked;
+      }
+    },
+  },
+
+  persist: true
 })
